517d75c4b25895dd5a41f991d6dfdb2d11121de2
Silverhawk <sondra.silverhawk@hush.com>
2017-03-18 00:20:10 -0700

    Add nav and header with some bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import "../stylesheets/main.scss";
 import { getArt } from '../actions';
 
+import NavBar from './NavBar';
+
 class App extends Component {
   componentWillMount() {
     this.props.getArt();
@@ -11,6 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+        <NavBar />
         {this.props.children}
       </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="page-home">
-        <h4>Outside Things!</h4>
+        <div className="jumbotron">
+          <h2>Outside Things!</h2>
+          <p className="lead">Find art or find trees</p>
+        </div>
         <ArtList />
       </div>
     );
